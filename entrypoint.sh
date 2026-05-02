7aecb27340467d68be65c2404f65fb97da46fbb0
Dhanashree Raut <daraut0203@gmail.com>
2026-05-03 05:11:08 +0530

    Fix PORT for Railway deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn jobboard.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn jobboard.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
