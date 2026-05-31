59ee6dfb2eb6e3123633b77f9833440e31b313a1
Dhanashree Raut <daraut0203@gmail.com>
2026-06-01 03:09:25 +0530

    Updated Docker files yml and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Starting server..."
-exec gunicorn jobboard.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
+echo "Starting: $@"
+exec "$@"
